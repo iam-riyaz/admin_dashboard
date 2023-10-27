c7effe6afadd94164ba319895753db81aa3bacb9
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2023-10-27 21:02:28 +0530

    code good practise

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -22,32 +22,31 @@ const Topbar = () => {
           backgroundColor={colors.primery[400]}
           borderRadius="3px"
         >
-
-            <InputBase sx={{ml:2,flex:1}} placeholder="Search"/>
-            <IconButton type="button" sx={{p:1}}>
-                <SearchIcon/>
-            </IconButton>
+          <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+          <IconButton type="button" sx={{ p: 1 }}>
+            <SearchIcon />
+          </IconButton>
         </Box>
         {/* ICONS */}
-         
-         <Box display="flex" >
-            <IconButton onClick={colorMode.toggleColorMode}>
-              {
-                theme.palette.mode==="dark"?<DarkModeOutlinedIcon/>:<LightModeOutlinedIcon/>
-              }
-            </IconButton>
-            <IconButton>
-                <NotificationsOutlinedIcon/>
-            </IconButton>
-            <IconButton>
-                 <SettingsOutlinedIcon/>
-            </IconButton>
-            <IconButton>
-                <PersonOutlinedIcon/>
-            </IconButton>
 
-         </Box>
-
+        <Box display="flex">
+          <IconButton onClick={colorMode.toggleColorMode}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlinedIcon />
+            ) : (
+              <LightModeOutlinedIcon />
+            )}
+          </IconButton>
+          <IconButton>
+            <NotificationsOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <PersonOutlinedIcon />
+          </IconButton>
+        </Box>
       </Box>
     </>
   );
