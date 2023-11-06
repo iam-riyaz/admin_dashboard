4d6afe2428d602c65fa753561b7c4d92add37a21
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2023-11-06 22:43:17 +0530

    form section added

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -1,12 +1,154 @@
-import { Box } from "@mui/material"
-import { Header } from "../../components/Header"
+import { Box, Button, TextField } from "@mui/material";
+import { Header } from "../../components/Header";
+import { Formik } from "formik";
+import * as yup from "yup";
+import {useMediaQuery} from "@mui/material";
 
-export const Form=()=>{
-    return(
-        <Box m="20px">
-              <Box display="flex">
-                <Header title="FORM" subtitle="welcome to form page" />
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: "",
+};
+
+const phoneRegExp = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+
+const userSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  contact: yup
+    .string()
+    .matches(phoneRegExp, "phone number is not valid")
+    .required("required"),
+  address1: yup.string().required("required"),
+  address2: yup.string().required("required"),
+});
+
+export const Form = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+  return (
+    <Box m="20px">
+      <Box display="flex">
+        <Header title="FORM" subtitle="welcome to form page" />
+      </Box>
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={userSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => {
+          return (
+            <form onSubmit={handleFormSubmit}>
+              <Box
+                display="grid"
+                gap="30px"
+                gridTemplateAreas="repeat(4, minmax(0,1fr))"
+                sx={{
+                  "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                }}
+              >
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="First Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  name="firstName"
+                  value={values.firstName}
+                  error={!!touched.firstName && !!errors.firstName}
+                  helperText={touched.firstName && errors.firstName}
+                  sx={{ gridColumn: "span 1" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Last Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  name="lastName"
+                  value={values.lastName}
+                  error={!!touched.lastName && !!errors.lastName}
+                  helperText={touched.lastName && errors.lastName}
+                  sx={{ gridColumn: "span 1" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  name="email"
+                  value={values.email}
+                  error={!!touched.email && !!errors.email}
+                  helperText={touched.email && errors.email}
+                  sx={{ gridColumn: "span 1" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="contact"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  name="contact"
+                  value={values.contact}
+                  error={!!touched.contact && !!errors.contact}
+                  helperText={touched.contact && errors.contact}
+                  sx={{ gridColumn: "span 1" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="address1"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  name="address1"
+                  value={values.address1}
+                  error={!!touched.address1 && !!errors.address1}
+                  helperText={touched.address1 && errors.address1}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="address2"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  name="address2"
+                  value={values.address2}
+                  error={!!touched.address2 && !!errors.address2}
+                  helperText={touched.address2 && errors.address2}
+                  sx={{ gridColumn: "span 2" }}
+                />
               </Box>
-       </Box>
-    )
-}
+              <Box display="flex" justifyContent="end" mt="20px">
+                <Button type="submit"  color="secondary" variant="contained">
+                  Create New User
+                </Button>
+              </Box>
+            </form>
+          );
+        }}
+      </Formik>
+    </Box>
+  );
+};
